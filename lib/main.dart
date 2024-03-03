fedd6d96d494b875d1dc400507bbb62261ab831b
Kristi <kristi@uni-bonn.de>
2024-03-03 14:02:30 +0100

    added utils and started pushing through the calendar example

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'dart:collection';
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
+import '../utils.dart';
 
 void main() {
-  runApp(TodoApp());
+  runApp(const TodoApp());
 }
 
 class TodoApp extends StatelessWidget {
@@ -17,7 +19,7 @@ class TodoApp extends StatelessWidget {
       ),
       initialRoute: '/',
       routes: {
-        '/': (context) => TodoScreen(),
+        '/': (context) => const TodoScreen(),
       },
     );
   }
@@ -41,7 +43,8 @@ class _TodoScreenState extends State<TodoScreen> {
             ? const Text('Calendar')
             : const Text('Todo List'),
       ),
-      body: _currentIndex == 0 ? CalendarScreen() : const TodoListScreen(),
+      body:
+          _currentIndex == 0 ? const CalendarScreen() : const TodoListScreen(),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentIndex,
         onTap: (index) {
@@ -86,48 +89,182 @@ class CalendarWidget extends StatefulWidget {
 }
 
 class _CalendarWidgetState extends State<CalendarWidget> {
-  // late CalendarController _calendarController;
+  late final ValueNotifier<List<Event>> _selectedEvents;
+  final ValueNotifier<DateTime> _focusedDay = ValueNotifier(DateTime.now());
+  final Set<DateTime> _selectedDays = LinkedHashSet<DateTime>(
+    equals: isSameDay,
+    hashCode: getHashCode,
+  );
+
+  late PageController _pageController;
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  RangeSelectionMode _rangeSelectionMode = RangeSelectionMode.toggledOff;
+  DateTime? _rangeStart;
+  DateTime? _rangeEnd;
 
   @override
   void initState() {
     super.initState();
-  }
 
-  @override
-  Widget build(BuildContext context) {
-    DateTime? _selectedDay;
-    DateTime? _focusedDay = DateTime.now();
-
-    final kToday = DateTime.now();
-    final kFirstDay = DateTime(kToday.year, kToday.month - 3, kToday.day);
-    final kLastDay = DateTime(kToday.year, kToday.month + 3, kToday.day);
-
-    return TableCalendar(
-      // calendarController: _calendarController,
-      calendarFormat: CalendarFormat.month,
-      startingDayOfWeek: StartingDayOfWeek.monday,
-      availableGestures: AvailableGestures.all,
-      calendarStyle: const CalendarStyle(),
-      headerStyle: const HeaderStyle(),
-      onDaySelected: (selectedDay, focusedDay) {
-        print("This day was selected $selectedDay");
-        if (!isSameDay(_selectedDay, selectedDay)) {
-          // Call `setState()` when updating the selected day
-          setState(() {
-            _selectedDay = selectedDay;
-            _focusedDay = focusedDay;
-          });
-        }
-      },
-      focusedDay: _focusedDay,
-      lastDay: kLastDay,
-      firstDay: kFirstDay,
-    );
+    _selectedDays.add(_focusedDay.value);
+    _selectedEvents = ValueNotifier(_getEventsForDay(_focusedDay.value));
   }
 
   @override
   void dispose() {
+    _focusedDay.dispose();
+    _selectedEvents.dispose();
     super.dispose();
+  }
+
+  bool get canClearSelection =>
+      _selectedDays.isNotEmpty || _rangeStart != null || _rangeEnd != null;
+
+  List<Event> _getEventsForDay(DateTime day) {
+    return kEvents[day] ?? [];
+  }
+
+  List<Event> _getEventsForDays(Iterable<DateTime> days) {
+    return [
+      for (final d in days) ..._getEventsForDay(d),
+    ];
+  }
+
+  List<Event> _getEventsForRange(DateTime start, DateTime end) {
+    final days = daysInRange(start, end);
+    return _getEventsForDays(days);
+  }
+
+  void _onDaySelected(DateTime selectedDay, DateTime focusedDay) {
+    setState(() {
+      if (_selectedDays.contains(selectedDay)) {
+        _selectedDays.remove(selectedDay);
+      } else {
+        _selectedDays.add(selectedDay);
+      }
+
+      _focusedDay.value = focusedDay;
+      _rangeStart = null;
+      _rangeEnd = null;
+      _rangeSelectionMode = RangeSelectionMode.toggledOff;
+    });
+
+    _selectedEvents.value = _getEventsForDays(_selectedDays);
+  }
+
+  void _onRangeSelected(DateTime? start, DateTime? end, DateTime focusedDay) {
+    setState(() {
+      _focusedDay.value = focusedDay;
+      _rangeStart = start;
+      _rangeEnd = end;
+      _selectedDays.clear();
+      _rangeSelectionMode = RangeSelectionMode.toggledOn;
+    });
+
+    if (start != null && end != null) {
+      _selectedEvents.value = _getEventsForRange(start, end);
+    } else if (start != null) {
+      _selectedEvents.value = _getEventsForDay(start);
+    } else if (end != null) {
+      _selectedEvents.value = _getEventsForDay(end);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('TableCalendar - Complex'),
+      ),
+      body: Column(
+        children: [
+          ValueListenableBuilder<DateTime>(
+            valueListenable: _focusedDay,
+            builder: (context, value, _) {
+              return _CalendarHeader(
+                focusedDay: value,
+                clearButtonVisible: canClearSelection,
+                onTodayButtonTap: () {
+                  setState(() => _focusedDay.value = DateTime.now());
+                },
+                onClearButtonTap: () {
+                  setState(() {
+                    _rangeStart = null;
+                    _rangeEnd = null;
+                    _selectedDays.clear();
+                    _selectedEvents.value = [];
+                  });
+                },
+                onLeftArrowTap: () {
+                  _pageController.previousPage(
+                    duration: Duration(milliseconds: 300),
+                    curve: Curves.easeOut,
+                  );
+                },
+                onRightArrowTap: () {
+                  _pageController.nextPage(
+                    duration: Duration(milliseconds: 300),
+                    curve: Curves.easeOut,
+                  );
+                },
+              );
+            },
+          ),
+          TableCalendar<Event>(
+            firstDay: kFirstDay,
+            lastDay: kLastDay,
+            focusedDay: _focusedDay.value,
+            headerVisible: false,
+            selectedDayPredicate: (day) => _selectedDays.contains(day),
+            rangeStartDay: _rangeStart,
+            rangeEndDay: _rangeEnd,
+            calendarFormat: _calendarFormat,
+            rangeSelectionMode: _rangeSelectionMode,
+            eventLoader: _getEventsForDay,
+            holidayPredicate: (day) {
+              // Every 20th day of the month will be treated as a holiday
+              return day.day == 20;
+            },
+            onDaySelected: _onDaySelected,
+            onRangeSelected: _onRangeSelected,
+            onCalendarCreated: (controller) => _pageController = controller,
+            onPageChanged: (focusedDay) => _focusedDay.value = focusedDay,
+            onFormatChanged: (format) {
+              if (_calendarFormat != format) {
+                setState(() => _calendarFormat = format);
+              }
+            },
+          ),
+          const SizedBox(height: 8.0),
+          Expanded(
+            child: ValueListenableBuilder<List<Event>>(
+              valueListenable: _selectedEvents,
+              builder: (context, value, _) {
+                return ListView.builder(
+                  itemCount: value.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin: const EdgeInsets.symmetric(
+                        horizontal: 12.0,
+                        vertical: 4.0,
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(),
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
+                      child: ListTile(
+                        onTap: () => print('${value[index]}'),
+                        title: Text('${value[index]}'),
+                      ),
+                    );
+                  },
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
 
