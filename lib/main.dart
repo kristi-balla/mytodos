1a30fe033dbe392ad3415e0445c45cc8011cbcad
Kristi <kristi@uni-bonn.de>
2024-03-03 13:41:53 +0100

    added a calendar layout without any functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 void main() {
   runApp(TodoApp());
 }
 
 class TodoApp extends StatelessWidget {
+  const TodoApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -21,6 +24,8 @@ class TodoApp extends StatelessWidget {
 }
 
 class TodoScreen extends StatefulWidget {
+  const TodoScreen({super.key});
+
   @override
   _TodoScreenState createState() => _TodoScreenState();
 }
@@ -36,8 +41,7 @@ class _TodoScreenState extends State<TodoScreen> {
             ? const Text('Calendar')
             : const Text('Todo List'),
       ),
-      body:
-          _currentIndex == 0 ? const CalendarScreen() : const TodoListScreen(),
+      body: _currentIndex == 0 ? CalendarScreen() : const TodoListScreen(),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentIndex,
         onTap: (index) {
@@ -66,8 +70,64 @@ class CalendarScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Center(
-      child: Text('Calendar Screen'),
+      child: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: CalendarWidget(),
+      ),
     );
+  }
+}
+
+class CalendarWidget extends StatefulWidget {
+  const CalendarWidget({super.key});
+
+  @override
+  _CalendarWidgetState createState() => _CalendarWidgetState();
+}
+
+class _CalendarWidgetState extends State<CalendarWidget> {
+  // late CalendarController _calendarController;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    DateTime? _selectedDay;
+    DateTime? _focusedDay = DateTime.now();
+
+    final kToday = DateTime.now();
+    final kFirstDay = DateTime(kToday.year, kToday.month - 3, kToday.day);
+    final kLastDay = DateTime(kToday.year, kToday.month + 3, kToday.day);
+
+    return TableCalendar(
+      // calendarController: _calendarController,
+      calendarFormat: CalendarFormat.month,
+      startingDayOfWeek: StartingDayOfWeek.monday,
+      availableGestures: AvailableGestures.all,
+      calendarStyle: const CalendarStyle(),
+      headerStyle: const HeaderStyle(),
+      onDaySelected: (selectedDay, focusedDay) {
+        print("This day was selected $selectedDay");
+        if (!isSameDay(_selectedDay, selectedDay)) {
+          // Call `setState()` when updating the selected day
+          setState(() {
+            _selectedDay = selectedDay;
+            _focusedDay = focusedDay;
+          });
+        }
+      },
+      focusedDay: _focusedDay,
+      lastDay: kLastDay,
+      firstDay: kFirstDay,
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
   }
 }
 
